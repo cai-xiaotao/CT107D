21a65d7c3192a62b11bb4a50110be4022f4d6e71
sanye5 <34118626+sanye5@users.noreply.github.com>
2020-03-30 18:57:00 +0800

    Add files via upload

---
+++ 国赛决赛/第三届/ds1302.c
@@ -0,0 +1,66 @@
+/*
+  程序说明: DS1302驱动程序
+  软件环境: Keil uVision 4.10 
+  硬件环境: CT107单片机综合实训平台 8051，12MHz
+  日    期: 2011-8-9
+*/
+
+#include <reg52.h>
+#include <intrins.h>
+
+sbit SCK=P1^7;		
+sbit SDA=P2^3;		
+sbit RST = P1^3;   // DS1302复位												
+
+void Write_Ds1302(unsigned  char temp) 
+{
+	unsigned char i;
+	for (i=0;i<8;i++)     	
+	{ 
+		SCK=0;
+		SDA=temp&0x01;
+		temp>>=1; 
+		SCK=1;
+	}
+}   
+
+void Write_Ds1302_Byte( unsigned char address,unsigned char dat )     
+{
+ 	RST=0;	_nop_();
+ 	SCK=0;	_nop_();
+ 	RST=1; 	_nop_();  
+ 	Write_Ds1302(address);	
+ 	Write_Ds1302(dat);		
+ 	RST=0; 
+}
+
+unsigned char Read_Ds1302_Byte ( unsigned char address )
+{
+ 	unsigned char i,temp=0x00;
+ 	RST=0;	_nop_();
+ 	SCK=0;	_nop_();
+ 	RST=1;	_nop_();
+ 	Write_Ds1302(address);
+ 	for (i=0;i<8;i++) 	
+ 	{		
+		SCK=0;
+		temp>>=1;	
+ 		if(SDA)
+ 		temp|=0x80;	
+ 		SCK=1;
+	} 
+ 	RST=0;	_nop_();
+ 	SCK=0;	_nop_();
+	SCK=1;	_nop_();
+	SDA=0;	_nop_();
+	SDA=1;	_nop_();
+	return (temp/16*10+temp%16);			
+}
+void set_time(unsigned char hour,unsigned char min,unsigned char sec)
+{
+	Write_Ds1302_Byte(0x8e,0x00);
+	Write_Ds1302_Byte(0x80,sec/10*16+sec%10);
+	Write_Ds1302_Byte(0x82,min/10*16+min%10);
+	Write_Ds1302_Byte(0x84,hour/10*16+hour%10);
+	Write_Ds1302_Byte(0x8e,0x80);
+}
